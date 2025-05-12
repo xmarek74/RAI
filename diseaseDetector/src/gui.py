5c4618f667cad9acbc839e0d6fdaccdb462f2e01
xmarek74 <xmarek74@vutbr.cz>
2025-05-13 01:52:47 +0200

    loading model via gui, added dialog pop-up

--- diseaseDetector/src/gui.py
+++ diseaseDetector/src/gui.py
@@ -1,25 +1,72 @@
 #imports
 import sys
+import os
 from keras._tf_keras.keras import models ,layers ,optimizers, preprocessing
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget, QFileDialog, QDesktopWidget
-from PyQt5.QtGui import QPixmap
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLineEdit, QLabel, QVBoxLayout, QWidget, QFileDialog, QDesktopWidget, QMessageBox, QHBoxLayout, QDialog
+from PyQt5.QtGui import QPixmap, QIcon
 from PyQt5.QtCore import Qt, QStandardPaths
 from os.path import dirname
 import numpy as np
+
+#class for pop-up after loading model
+class CustomDialog(QDialog):
+    def __init__(self, title, message, parent=None):
+        super().__init__(parent)
+        #init atributes
+        self.setWindowTitle(title)
+        self.setWindowFlag(Qt.WindowContextHelpButtonHint, False)
+        self.setFixedSize(400, 160)
+
+        layout = QVBoxLayout()
+        layout.setContentsMargins(40, 20, 40, 20)
+        layout.setSpacing(10)
+
+        #set up horizontal layout (flex look-a-like) for icon and text
+        innerLayout = QHBoxLayout()
+        iconLabel = QLabel()
+        icon = QIcon("../public/icons/ok.png")
+        iconPixmap = icon.pixmap(65, 65)
+        iconLabel.setPixmap(iconPixmap)
+        iconLabel.setFixedWidth(65)
+
+        messageLabel = QLabel(message)
+        messageLabel.setObjectName("messageText")
+        messageLabel.setWordWrap(True)
+        messageLabel.setAlignment(Qt.AlignVCenter | Qt.AlignRight)
+
+        innerLayout.addWidget(iconLabel)
+        innerLayout.addWidget(messageLabel, 1)
+        
+
+        layout.addLayout(innerLayout)
+
+        #confirmation button
+        okButton = QPushButton("Continue")
+        okButton.setObjectName("messageButton")
+        okButton.clicked.connect(self.accept)
+        okButton.setCursor(Qt.PointingHandCursor)
+        layout.addWidget(okButton, alignment=Qt.AlignRight)
+
+        self.setLayout(layout)
 
 #main class representing gui
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
+        #init attributes
+        self.model = None
+        self.filePath = None
+        self.result = None
         #set up path for loading images
         self.dir = QStandardPaths.writableLocation(QStandardPaths.DesktopLocation)
         #set up window centered
-        self.setWindowTitle("PyQt5 Plant Disease Detection")
-        self.setGeometry(0, 0, 400, 400)
+        self.setWindowTitle("Plant Disease Detection")
+        self.setGeometry(0, 0, 400, 600)
         screen = QDesktopWidget().availableGeometry()
         screenWidth = screen.width()
         screenHeight = screen.height()
-        self.move((screenWidth - 300) // 2, (screenHeight-200) // 2)
+        self.move((screenWidth - 400) // 2, (screenHeight-600) // 2)
         #create layout of app
         centralWidget = QWidget(self)
         self.setCentralWidget(centralWidget)
@@ -28,8 +75,15 @@ class MyWindow(QMainWindow):
         layout.setSpacing(20)
         #add components to layout
         self.buttonLoad = QPushButton("Load image", self)
-        self.buttonLoad.setObjectName("loadButton")
+        self.buttonLoad.setObjectName("guiButton")
+        self.buttonLoad.setCursor(Qt.PointingHandCursor)
         layout.addWidget(self.buttonLoad)
+
+        self.buttonLoadModel = QPushButton("Load model", self)
+        self.buttonLoadModel.setObjectName("guiButton")
+        self.buttonLoadModel.setCursor(Qt.PointingHandCursor)
+
+        layout.addWidget(self.buttonLoadModel)
 
         self.imageLabel = QLabel(self)
         self.imageLabel.setObjectName("imageArea")
@@ -38,26 +92,43 @@ class MyWindow(QMainWindow):
 
         self.buttonAnalyze = QPushButton("Analyze image", self)
         self.buttonAnalyze.setEnabled(False)
+        self.buttonAnalyze.setObjectName("guiButton")
+        self.buttonAnalyze.setCursor(Qt.PointingHandCursor)
+
         layout.addWidget(self.buttonAnalyze)
 
         #connect compononents to functions
         self.buttonLoad.clicked.connect(self.loadImage)
         self.buttonAnalyze.clicked.connect(self.analyzeImage)
+        self.buttonLoadModel.clicked.connect(self.loadModel)
 
     def loadImage(self):
-        fileName, _ = QFileDialog.getOpenFileName(self, "Open Image", self.dir, "Images Files (*.png *.jpg *.bmp *.jpeg)")
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Image", self.dir, "Images Files (*.png *.jpg *.jpeg)")
         if fileName:
             pixmap = QPixmap(fileName)
             self.imageLabel.setPixmap(pixmap.scaled(self.imageLabel.size(), Qt.IgnoreAspectRatio, Qt.SmoothTransformation))
-            self.buttonAnalyze.setEnabled(True)
+            if (self.model):
+                self.buttonAnalyze.setEnabled(True)
             self.filePath = fileName
             #set last used as current location
             self.dir = dirname(fileName)
-    #TODO connect with main CNN
+    def loadModel(self):
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Model", self.dir, "Keras files (*.h5 *.keras)")
+        if fileName:
+            self.model = fileName
+            alert = CustomDialog("Success", "Model has been loaded successfully", self)
+            if (self.filePath):
+                self.buttonAnalyze.setEnabled(True)
+        else:
+            #TODO test if error
+            alert = CustomDialog("Error", "Model couldn't be loaded", self)
+        alert.exec_()
+
     def analyzeImage(self):
         #static for now
-        model = models.load_model("diseaseModel.h5")
-        file = preprocessing.image.load_img(self.filePath, target_size=(128, 128))
+        model = models.load_model(self.model)
+        
+        file = preprocessing.image.load_img(self.filePath, target_size=(128, 128)) 
         #normalize rgb
         fileArray = preprocessing.image.img_to_array(file) / 255.0
         #add dim
