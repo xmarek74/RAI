977e36967c07131590c17c7f67a5217a28f7ecdf
xmarek74 <xmarek74@vutbr.cz>
2025-05-21 16:46:45 +0200

    trained best version of model, added history printing

--- diseaseDetector/src/main.py
+++ diseaseDetector/src/main.py
@@ -1,10 +1,12 @@
 import os
-os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+os.environ["TF_ENABLE_ONEDNN_OPTS"] = "0"
 #directly import keras cuz pylance is blind - https://github.com/microsoft/pylance-release/issues/1066
 from keras._tf_keras.keras import models ,layers ,optimizers, preprocessing, callbacks, regularizers, metrics
 import matplotlib.pyplot as plt
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.utils import class_weight
+from datetime import datetime
+import json
 import seaborn as sns
 import numpy as np
 import argparse
@@ -19,21 +21,22 @@ valData = preprocessing.image.ImageDataGenerator(rescale=1./255)
 def trainModel(modelName):
 
     trainGen = trainData.flow_from_directory(
-        '../public/train',
+        "../public/train",
         target_size=(64, 64),
         batch_size=64,
-        class_mode='categorical',
+        class_mode="categorical",
         shuffle=True
     )
 
     valGen = valData.flow_from_directory(
-        '../public/val',
+        "../public/val",
         target_size=(64, 64),
         batch_size=64,
-        class_mode='categorical',
+        class_mode="categorical",
         shuffle=False
     )
 
+    #since classes are not balanced, use class_weight => model keeps more attention to class with less images
     classWeight = dict(zip(np.unique(trainGen.classes), class_weight.compute_class_weight(class_weight="balanced", classes=np.unique(trainGen.classes), y=trainGen.classes)))
     print(classWeight)
     #0.3 0.3 0.3 0.4 0.4 => valF1Score = 0.85 (precision 0.76)
@@ -59,22 +62,27 @@ def trainModel(modelName):
         layers.Flatten(),
         layers.Dense(128),
         layers.BatchNormalization(),
-        layers.Activation('relu'),
+        layers.Activation("relu"),
         layers.Dropout(0.4),
         
         layers.Dense(64),
         layers.BatchNormalization(),
-        layers.Activation('relu'),
+        layers.Activation("relu"),
         layers.Dropout(0.4),
-        layers.Dense(trainGen.num_classes, activation='softmax')
+        layers.Dense(trainGen.num_classes, activation="softmax")
     ])
 
-    model.compile(optimizer=optimizers.Adam(learning_rate=0.00005), loss='categorical_crossentropy', metrics=['accuracy', "Precision", "Recall", metrics.F1Score(average="weighted", name="F1Score")])
+    model.compile(optimizer=optimizers.Adam(learning_rate=0.0001), loss="categorical_crossentropy", metrics=["Accuracy", "Precision", "Recall", metrics.F1Score(average="weighted", name="F1Score")])
 
-    history = model.fit(trainGen, validation_data=valGen, epochs=30, class_weight=classWeight,
+    #save to history to display model's evaluation
+    history = model.fit(trainGen, validation_data=valGen, epochs=20, class_weight=classWeight,
               callbacks=[callbacks.EarlyStopping(patience=5, restore_best_weights=True, monitor="val_F1Score", verbose=1),
                          callbacks.ModelCheckpoint("best_" + modelName, monitor="val_F1Score", mode="max", save_best_only=True, verbose=1),
-                         callbacks.ReduceLROnPlateau(monitor="val_F1Score", factor=0.5, patience=2, verbose=1, min_lr=1e-6)])
+                         callbacks.ReduceLROnPlateau(monitor="val_F1Score", factor=0.5, patience=3, verbose=1, min_lr=1e-6)])
+    time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    name = f"history_{time}.json"
+    with open(name, "w") as f:
+        json.dump(history.history, f) 
     model.save(modelName)
     return model, history
 
@@ -87,10 +95,10 @@ def evaluateModel(modelPath):
         return 1
 
     valGen = valData.flow_from_directory(
-        '../public/val',
-        target_size=(128, 128),
-        batch_size=16,
-        class_mode='categorical',
+        "../public/val",
+        target_size=(64, 64),
+        batch_size=64,
+        class_mode="categorical",
         shuffle=False
     )
     predictions = model.predict(valGen)
@@ -112,6 +120,47 @@ def evaluateModel(modelPath):
     plt.title("Confusion Matrix")
     plt.show()
     return 0
+
+def printHistory(path):
+    with open(path, "r") as f:
+        history = json.load(f)
+    keys = list(history.keys())
+    trainMetrics = []
+    valMetrics = []
+    for k in keys:
+        if (not k.startswith("learning_rate")) and (not k.startswith("val_")):
+            trainMetrics.append(k)
+        elif k.startswith("val_"):
+            valMetrics.append(k)
+
+    #display train vs val metric as a comparison
+    for metric in trainMetrics:
+        plt.figure(figsize=(8, 5))
+        plt.plot(history[metric], label= metric, marker="o")
+
+        for val in valMetrics:
+            if metric in val:
+                plt.plot(history[val], label=f"val_{metric}", marker="o")                
+
+
+        plt.title(f"{metric.capitalize()} over epochs")
+        plt.xlabel("Epoch")
+        plt.ylabel(metric.capitalize())
+        plt.legend()
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
+    if ("learning_rate" in history):
+        plt.figure(figsize=(8, 4))
+        plt.plot(history["learning_rate"], label="Learning Rate", color="purple", marker="o")
+        plt.title("Learning Rate Schedule")
+        plt.xlabel("Epoch")
+        plt.ylabel("Learning Rate")
+        plt.grid(True)
+        plt.tight_layout()
+        plt.show()
+
 
 def printHelp():
     print("""
@@ -147,13 +196,22 @@ if __name__ == "__main__":
     parser.add_argument("-e", "--eval", help="Evaluates accuracy of model", required=False)
     parser.add_argument("-t", "--train", help="Trains model and saves it as selected name, could take up to 30 minutes", required=False)
     parser.add_argument("-u", "--usage", help="Prints help on how to use the script", required=False, action="store_true")
+    parser.add_argument("-his", "--history", help="Prints history graphs based on model's learning", required=False)
     args = parser.parse_args()
-    if (countArgs([args.eval, args.train, args.usage]) == 1):
+    if (countArgs([args.eval, args.train, args.usage, args.history]) == 1):
         if args.eval:
-            evaluateModel(args.eval)
+            if (not evaluateModel(args.eval)):
+                sys.exit(0)
+            else:
+                sys.exit(1)
         elif args.train and args.train.endswith((".h5", ".keras")):
             trainModel(args.train)
+            sys.exit(0)
+        elif args.history and args.history.endswith(".json"):
+            printHistory(args.history)
+            sys.exit(1)
         else:
             printHelp()
+            sys.exit(0)
     else:
         printHelp()
